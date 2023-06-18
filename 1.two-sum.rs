cf5d62effb2409b2ff9ad3d510b9beb907f06a51
web3nomad.eth <me@web3nomad.eth>
2023-06-19 03:07:35 +0800

    update two sum

--- 1.two-sum.rs
+++ 1.two-sum.rs
@@ -6,16 +6,19 @@
 
 // @lc code=start
 impl Solution {
-  pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    // loop through the vector nums
-    for i in 0..nums.len() {
-      for j in (i + 1) .. nums.len() {
-        if nums[i] + nums[j] == target {
-          return vec![i as i32, j as i32];
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        // solve leetcode two sum problem in O(n)
+        // time complexity and O(n) space complexity
+        use std::collections::HashMap;
+        let mut map = HashMap::new();
+        for (i, &num) in nums.iter().enumerate() {
+            let complement = target - num;
+            if map.contains_key(&complement) {
+                return vec![*map.get(&complement).unwrap() as i32, i as i32];
+            }
+            map.insert(num, i);
         }
-      }
+        panic!("No two sum solution");
     }
-    return vec![0, 0];
-  }
 }
 // @lc code=end
