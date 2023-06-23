a953096236b6b59a2216ee3cf54c36696664c472
web3nomad.eth <me@web3nomad.eth>
2023-06-23 18:56:52 +0800

    solve sol_9_palindrome_number

--- solutions/sol_9_palindrome_number.rs
+++ solutions/sol_9_palindrome_number.rs
@@ -9,6 +9,18 @@ pub struct Solution;
 // @lc code=start
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
+        if x < 0 {
+            return false;
+        }
+        let str_of_x: Vec<char> = x.to_string().chars().collect();
+        let len = str_of_x.len();
+        let mut i = 0;
+        while i < len / 2 {
+            if str_of_x[i] != str_of_x[len - i - 1] {
+                return false;
+            }
+            i += 1;
+        }
         true
     }
 }
@@ -20,6 +32,8 @@ mod tests {
     
     #[test]
     fn it_works() {
-        // 
+        assert_eq!(Solution::is_palindrome(-131), false);
+        assert_eq!(Solution::is_palindrome(131), true);
+        assert_eq!(Solution::is_palindrome(132), false);
     }
 }
