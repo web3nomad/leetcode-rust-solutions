f95e9ed9439101779fe910cb92fe2578d30e8500
web3nomad.eth <me@web3nomad.eth>
2023-06-24 21:12:33 +0800

    add alternate impl of add_two_numbers

--- solutions/sol_2_add_two_numbers.rs
+++ solutions/sol_2_add_two_numbers.rs
@@ -69,6 +69,31 @@ impl Solution {
         }
         root
     }
+
+    pub fn add_two_numbers_alt(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        match (l1, l2) {
+            (None, None) => None,
+            (Some(n), None) | (None, Some(n)) => Some(n),
+            (Some(n1), Some(n2)) => {
+                let sum = n1.val + n2.val;
+                if sum < 10 {
+                    Some(Box::new(ListNode {
+                        val: sum,
+                        next: Solution::add_two_numbers(n1.next, n2.next),
+                    }))
+                } else {
+                    let carry = Some(Box::new(ListNode::new(1)));
+                    Some(Box::new(ListNode {
+                        val: sum - 10,
+                        next: Solution::add_two_numbers(
+                            Solution::add_two_numbers(carry, n1.next),
+                            n2.next,
+                        ),
+                    }))
+                }
+            }
+        }
+    }
 }
 // @lc code=end
 
